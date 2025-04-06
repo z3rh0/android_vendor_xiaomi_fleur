8f4b75d2798a9b8da9fef20253aad315a9a27503
claxten10 <claxten10@gmail.com>
2025-04-06 14:29:23 +0000

    fleur: Initial proprietary files
    Signed-off-by: claxten10 <claxten10@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,21 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),fleur)
+
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,a1b8da571c4126daad68bf1124c6b677f4077588)
+$(call add-radio-file-sha1-checked,radio/gz.img,19e4e5e09c506b5784843dc92f5a8ad2bae5eac6)
+$(call add-radio-file-sha1-checked,radio/lk.img,da8fbda904364da7c24adcbe49ae1aaccadf8696)
+$(call add-radio-file-sha1-checked,radio/logo.img,d06b5bc5139783f4da2dbd4be6e2a34e73a9c0de)
+$(call add-radio-file-sha1-checked,radio/md1img.img,367367494064f983f72239fc23a0fc4518dbcdd9)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,396292043395879c236e43eba15fe3f94a98d505)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,abb80a587ee1ebad3801853de46ee5d0efc6a41b)
+$(call add-radio-file-sha1-checked,radio/scp.img,6d013815d35b4ed1f9726a5f6c3aae8195ea70a6)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,4447305e6daccfb4189e684aad295b9b0372deb2)
+$(call add-radio-file-sha1-checked,radio/sspm.img,7b02fef9f977192311c9f861e18355b179222d69)
+$(call add-radio-file-sha1-checked,radio/tee.img,bf886717a3230305e1cd451a5717710cd95af1a2)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,16 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    audio_dsp \
+    gz \
+    lk \
+    logo \
+    md1img \
+    pi_img \
+    preloader_raw \
+    scp \
+    spmfw \
+    sspm \
+    tee
